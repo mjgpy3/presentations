28a429c0151fe258f4c5dc850af4e8b47e5ca1c5
Michael Gilliland <mjg.py3@gmail.com>
2016-02-22 23:16:42 -0500

    Finish final FP examples :)

---
+++ intro_to_fp_in_js/examples/optionals.js
@@ -0,0 +1,92 @@
+var _ = require('lodash');
+
+function Just(v) {
+  this.thenDo = function (f) {
+    return f(v);
+  };
+
+  this.asList = [v];
+}
+
+function Nothing() {
+  this.thenDo = function (_) {
+    return new Nothing();
+  };
+
+  this.asList = [];
+}
+
+function catOptionals(optionals) {
+  return _.flatten(
+    _.map(optionals, 'asList')
+  );
+}
+
+var grandma = {
+  name: 'Uma',
+  father: new Nothing(),
+  mother: new Nothing()
+};
+
+var grandpap = {
+  name: 'Cletus',
+  father: new Nothing(),
+  mother: new Nothing()
+};
+
+var dad = {
+  name: 'Roger',
+  father: new Just(grandpap),
+  mother: new Just(grandma)
+};
+
+var jim = {
+  name: 'Jimmy',
+  father: new Just(dad),
+  mother: new Nothing()
+};
+
+function concatMap(f, items) {
+  return _.flatten(
+    _.map(items, f)
+  );
+}
+
+function parents(person) {
+    return person.father.asList.concat(
+      person.mother.asList
+    );
+}
+
+function grandparents(person, nGreat) {
+  if (nGreat === -2) {
+    return person;
+  }
+
+  return concatMap(
+    nBackGrandparent(nGreat - 1),
+    parents(person)
+  );
+}
+
+function nBackGrandparent(n) {
+  return function (person) {
+    return grandparents(person, n);
+  };
+}
+
+function path(person, selectors) {
+  if (selectors.length === 0) {
+    return new Just(person);
+  }
+
+  return person[selectors[0]].thenDo(function (parent) {
+    return path(parent, _.drop(selectors, 1));
+  });
+}
+
+console.log(parents(jim));
+console.log('------------------');
+console.log(grandparents(jim, 0));
+console.log('------------------');
+console.log(path(jim, ['father', 'mother']));
