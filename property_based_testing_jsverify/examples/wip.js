b7881e4189d910c3688ff3a275e33fae9d7c4408
Michael Gilliland <mjg.py3@gmail.com>
2016-06-06 08:47:10 -0600

    Cleaning up

--- property_based_testing_jsverify/examples/wip.js
+++ property_based_testing_jsverify/examples/wip.js
@@ -39,13 +39,13 @@ jsc.assert(
   abs_is_idempotent
 );
 
-function ifPersonLike(value, personlikeCallback, otherwiseCallback) {
-  if (value.name && value.age && value.address) {
+function isPersonLike(value, personlikeCallback, otherwiseCallback) {
+  if (value.name && value.age && value.age >= 0 && value.address) {
     return personlikeCallback(value);
   }
 
   return otherwiseCallback(value);
-} 
+}
 
 var validPerson = jsc.record({
   name: jsc.string,
@@ -53,15 +53,20 @@ var validPerson = jsc.record({
   address: jsc.string
 });
 
-var examplePersonCallback = jsc.fn(validPerson, );
+var examplePersonCallback = jsc.fn(validPerson, jsc.nat);
+var exampleOtherwiseCallback = jsc.fn(jsc.nat);
 
 var ifpersonlike_invokes_the_first_callback_given_a_person =
 
   jsc.forall(
-    validPersonType
-    , function (v) {
-    console.log(v); 
-    return true;
+    validPerson,
+    examplePersonCallback,
+    exampleOtherwiseCallback
+    , function (v, f, g) {
+    console.log(v);
+    console.log('f(v)', f(v));
+    console.log('ifPersonLike(v, f, g)', ifPersonLike(v, f, g));
+    return ifPersonLike(v, f, g) === f(v);
   });
 
 jsc.assert(
